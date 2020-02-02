bbacdf5e19db8fe8759b0570e26db0f394e5c3a8
luis <djmasakre@gmail.com>
2020-02-02 12:03:16 +0100

    audio file parser working

--- app/index.js
+++ app/index.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 const utils = require('./utils');
 
 console.log(' mp3DirMetadata says hello! ');
 
-const musicFolder = '/home/luis/Música/';
+const musicFolder = process.argv[2]  || ( os.type() === 'Darwin' ) ? 
+                                          '/Users/luis/Downloads' :
+                                          '/home/luis/Música/DANI/Zanthrax- Lost heritage';
+
+if ( !fs.lstatSync(musicFolder).isDirectory() ) {
+  console.error( 'provided music folder is not a directory', musicFolder);
+  return;
+}
+
 
 utils.scanDir4audioFiles(musicFolder);
 
--- app/utils.js
+++ app/utils.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const mm = require('music-metadata');
+const mm = require('musicmetadata');
 const util = require('util')
 
 // CONFIG
@@ -24,43 +24,48 @@ function handleDirFiles (err, files, dirPath)
 {
   files.forEach(file => {
 
-      file = dirPath + file;
+      file = dirPath +'/'+ file;
 
       if (fs.lstatSync(file).isFile() && supportedFileTypes.includes(path.extname(file))) {
-        console.log(' %c>> Music file detected ', 'color:yellow, font-size:13px', file);
         handleAudioFile(file);
-        console.log('');
       }
-
+      
       if ( fs.lstatSync(file).isDirectory() ) {
         console.log( 'DIRECTORY DETECTED' , file);        
       }
-     
+      
     });
   }
-
-
-
+  
+  
+  
   function handleAudioFile(filePath) 
   {
-    mm.parseFile(filePath)
-      .then(metadata => {
-        console.log('metadata', metadata);        
-        // console.log('metadata util.inspect', util.inspect(metadata, { showHidden: false, depth: null }));
+    var parser = mm(fs.createReadStream(filePath), function (err, metadata) {
+      if (err) throw err;
 
-        if ( metadata.format.trackInfo.length ) {
+      console.log(filePath + ' metadata:',  metadata);
+    });
+    // mm.parseFile(filePath)
+    // .then(metadata => {
+    //     console.log('>> Music file detected ', filePath);
+    //     console.log('>>>> metadata', metadata);        
+    //     // console.log('metadata util.inspect', util.inspect(metadata, { showHidden: false, depth: null }));
 
-        } else {
-          
-          const fileName = path.basename(filePath);
-
-        }
+    //     const fileName =  ( metadata.format.trackInfo.length ) ? JSON.stringify(metadata.format.trackInfo) : path.basename(filePath);
+    //     console.log( '>>>> filename', fileName);
+    //     console.log('');
 
 
-      })
-      .catch(err => {
-        console.error(err.message);
-      });
+    //   })
+    //   .catch(err => {
+    //     console.error('!handleAudioFile Error', { 
+    //       message: err.message,
+    //       file: filePath,
+    //       err:err
+    //     });
+    //     console.log('');
+    //   });
   }
 
   
